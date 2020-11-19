19b470661dbf51e297bd0b03d52bc245e27be5c7
Naveen Nersu <nnersu@cisco.com>
2020-11-19 23:04:22 +0000

    update project id

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "auto-infra-20201119-student10x-tfstate"
-    credentials = "/home/student10/20201119/tf-labs/jenkins-sa.json"
+    credentials = "./creds/jenkins-sa.json"
   }
 }
